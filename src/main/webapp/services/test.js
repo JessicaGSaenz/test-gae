c7779201a5b9ebce05c3c19e7f62c5978d0a1688
Gonzalez Saenz <saenz@atsistemas.com>
2019-02-14 17:11:39 +0100

    Se añade la vista details

--- src/main/webapp/services/test.js
+++ src/main/webapp/services/test.js
@@ -5,21 +5,20 @@ angular.module('book')
         return {
             list: function (success) {
             	//return $http.get("/_ah/remote_api/book").then(success);
-                //return $http.get("/rest/book").then(success);
             	return $http.get("/remote_api/book").then(success);
             },
             save: function (book, success) {
             	//return $http.post("/rest/book", book).then(success);
                 return $http.post("/remote_api/book", book).then(success);
-            }/*,
+            },
             delete: function (id, success) {
-                return $http.delete("/rest/book/"+id).then(success);
+                return $http.delete("/remote_api/book/"+id).then(success);
             },
             get: function (id, success) {
-                return $http.get("/rest/book/"+id).then(success);
+                return $http.get("/remote_api/book/"+id).then(success);
             },
             search: function (text, success) {
-                return $http.get("/rest/book/search/"+text).then(success);
-            }*/
+                return $http.get("/remote_api/book/searchBook/"+text).then(success);
+            }
         };
     });
